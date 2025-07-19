f550cbabf9331ab032677a3622d22dbf037d4734
Day Reez <nalumaliyil@gmail.com>
2025-07-19 14:48:46 +0530

    Folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+// import { useState } from 'react'
+import TourCompanyCard from './TourCompanyCard.jsx';
+import companyLogo from './assets/react.svg';
+import LoginPage from './LoginPage.jsx';
+
+function App() {
+
+  return(
+    
+    // <div style={{display: 'flex', gap: '1rem'}}>
+    //   {/* <TourCompanyCard
+    //   companyLogo={companyLogo}
+    //   companyName="Angelina Rose" 
+    //   companyLocation="Kozhikode"
+    //   companyDescription = "Discover unforgettable journeys with us — where every trip becomes a story worth telling."
+    //   /> */}
+
+    //   {/* <TourCompanyCard
+    //   companyLogo={companyLogo}
+    //   companyName="Boom" 
+    //   companyLocation="Kerala"
+    //   companyDescription = "hello"
+      
+    //   />
+
+    //   <TourCompanyCard
+    //   companyLogo={companyLogo}
+    //   companyName="Boom" 
+    //   companyLocation="Kerala"
+    //   companyDescription = "hello"
+    //   /> */}
+
+    // </div>
+
+    <LoginPage />
+
+  );
+}
+
+export default App
