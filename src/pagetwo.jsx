507b35ff603316b44cf2aa48ef6c2be1783e3401
biju mv <anjlinarose1001@gmail.com>
2025-07-19 15:07:20 +0530

    feat: add Header component to App layout This commit integrates the Header component into the main App layout, preparing the structure for further UI development.

---
+++ src/pagetwo.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header.jsx"
+import ImageOverlayCard from './ImageOverlayCard.jsx'
+import Suggestions from "./suggestions.jsx";
+
+function App() {
+  
+    return(
+      <>
+        <Header />
+        <ImageOverlayCard/>
+        <Suggestions/>
+      </>
+      
+    );
+}
+
+export default App
